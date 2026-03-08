87a2da36563e4afaf79a9d76a5a8a448ba2a36ca
Julio-Glez <julio.glezjim@gmail.com>
2026-03-08 15:39:36 -0600

    Reverse automatico

--- reversestring-JGJ/script.js
+++ reversestring-JGJ/script.js
@@ -1,18 +1,9 @@
-function toggleButton() {
-    const input = document.getElementById("inputText").value;
-    const button = document.getElementById("reverseBtn");
-
-    if (input.length > 3) {
-        button.style.display = "inline";
-    } else {
-        button.style.display = "none";
-    }
-}
-
 function reverseText() {
+
     const input = document.getElementById("inputText").value;
 
     const reversed = input.split("").reverse().join("");
 
     document.getElementById("result").textContent = reversed;
+
 }
