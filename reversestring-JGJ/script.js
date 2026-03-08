2772ce31d1cdcfef1f95fdff06da5515e4286249
Julio-Glez <julio.glezjim@gmail.com>
2026-03-08 15:32:08 -0600

    Boton invisible

--- reversestring-JGJ/script.js
+++ reversestring-JGJ/script.js
@@ -1,3 +1,14 @@
+function toggleButton() {
+    const input = document.getElementById("inputText").value;
+    const button = document.getElementById("reverseBtn");
+
+    if (input.length > 3) {
+        button.style.display = "inline";
+    } else {
+        button.style.display = "none";
+    }
+}
+
 function reverseText() {
     const input = document.getElementById("inputText").value;
 
